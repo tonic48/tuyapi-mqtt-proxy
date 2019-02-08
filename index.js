cb058ec75211d7a12f94ed60d715b9507c7f0b77
Superman <tonic48@gmail.com>
2019-02-09 00:37:51 +0100

    commit

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const mqtt = require('mqtt')
 
 var configTuyapi = require("./conf/tuyapi.json");
 var configMqtt = require("./conf/mqtt.json");
+var topics = require("./conf/topics.json");
 
+const mqttClient = mqtt.connect(configMqtt.url,configMqtt.options);
 const device = new TuyAPI(configTuyapi);
+
 
 device.on('connected',() => {
   console.log('Connected to device.');
@@ -17,10 +20,21 @@ device.on('disconnected',() => {
 
 device.on('data', data => {
   console.log('Data from device:', data);
-  const status = data.dps['1'];
-  device.set({set: "50", dps: "2"});
+  //const status = data.dps['1'];
+  //console.log('Current status:', status);
+  var payload="";
+  for(var key in data.dps) {
+   payload=""+data.dps[key];
+   switch (key){
+     case '2':
+     case '3':
+     case '102':
+        break;
+      }
 
-  console.log('Current status:', status);
+       mqttClient.publish(""+topics[key],payload);
+   }
+
 
 });
 
@@ -31,18 +45,16 @@ device.on('error',(err) => {
 device.connect();
 
 // Disconnect after 10 seconds
-setTimeout(() => { device.disconnect(); }, 1000000);
-
-var mqttClient = mqtt.connect(configMqtt.url,configMqtt.options);
+setTimeout(() => { device.disconnect(); }, 100000);
 
 mqttClient.on('connect', () => {
   console.log('Connected to MQTT.');
-  mqttClient.subscribe('cmd/thermostat/temp')
+  mqttClient.subscribe('cmd/floor_thermostat/temp_set')
 
 })
 
 mqttClient.on('message', (topic, message) => {
-  if(topic === 'cmd/thermostat/temp') {
+  if(topic === 'cmd/floor_thermostat/temp_set') {
     var setTemp = parseInt(message)
     console.log('Set temperature to : '+setTemp);
     if(setTemp>=10 && setTemp<=70){
