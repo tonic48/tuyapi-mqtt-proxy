51f372d0ec7458097b87abf66d1350fc323daace
tonic48 <anton.tsyvyan@blujaysolutions.com>
2019-12-30 14:30:36 +0100

    Changes

--- index.js
+++ index.js
@@ -1,49 +1,10 @@
-const TuyAPI = require('tuyapi');
 const mqtt = require('mqtt')
-var debug = require('debug');
-const SerialPort = require('serialport')
-const configMqtt = require("./conf/mqtt.json");
-const dpsCodes = require("./conf/dps-code.json");
-//define dps constants
-const configBrewapi = require("./conf/brewapi.json");
+const debug = require('debug')('TuyAPI-Proxy');
+const configTuyapi = require("./conf/tuyapi.json");
+const TuyaItem = require("./tuya-item.js");
 
-//Start mqtt client
-var mqttClientDaemon = mqtt.connect(configMqtt.url, configMqtt.options);
-console.log('mqtt client has been started');
+console.log("Debug level : "+process.env.DEBUG);
+Object.keys(configTuyapi).forEach(key=>{
+   var device = new TuyaItem(configTuyapi[key]);
+});
 
-//subscribe to all command topics
-mqttClientDaemon.subscribe(getSubTopics("cmd"),(err, granted)=>{
-   if(!err){
-     console.log("Subscribed to ");
-     Object.keys(granted).forEach(key=>{console.log(granted[key])});
-   }
-
- });
-
- //subscribe to all command topics
-mqttClientDaemon.subscribe(getSubTopics("cmd"),(err, granted)=>{
-   if(!err){
-     console.log("Subscribed to ");
-     Object.keys(granted).forEach(key=>{console.log(granted[key])});
-   }
-
- });
-
- mqttClientDaemon.on("reconnect", (err) => {
-   console.log("Reconnect to MQTT");
- });
- 
- mqttClientDaemon.on("error", (err) => {
-   console.log("Error connection MQTT");
- });
- 
- mqttClientDaemon.on('message', (topic, message) => {
-   console.log(topic +" ; "+message);
- });
-
-
- function getSubTopics(pref){
-   var topics = new Object();
-   topics[pref+"/"+configBrewapi.device+"/"+configBrewapi.current_temp_topic]=0;
-   return topics;
- };
